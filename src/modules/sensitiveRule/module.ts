c4dad743b3f041fa64b2bc26606e4e95b7ae7d1c
yyjb5 <yyjb5@163.com>
2022-07-02 15:19:52 +0800

    fix: 修改模块之前的依赖

--- src/modules/sensitiveRule/module.ts
+++ src/modules/sensitiveRule/module.ts
@@ -2,14 +2,8 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { SensitiveRuleservice } from './service';
 import { SensitiveRulesResolver } from './resolver';
-import { ClassificationTemplateService } from '../classificationTemplate/service';
 
 @Module({
-  providers: [
-    PrismaService,
-    SensitiveRuleservice,
-    ClassificationTemplateService,
-    SensitiveRulesResolver,
-  ],
+  providers: [PrismaService, SensitiveRuleservice, SensitiveRulesResolver],
 })
 export class SensitiveRuleModule {}
